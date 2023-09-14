85201a472ae64ed22572146d0c1d9c02f7e27259
OUSSAMA FILALI <ouss-19998@outlook.fr>
2023-09-14 11:38:17 -0700

    ready

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -2,12 +2,9 @@
 
 void positive_or_negative(int i)
 {
-   if (i > 0)
-           printf("%d is positive\n", i);
-
-   else if (n < 0)
-           printf("%d is negative\n", i);
-
-   else 
-	   printf("%d is zero\n", i);
-}
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
+	else
+		printf("%d is negative\n", i);
